0c04fa8b0ac2db6ec09d151a9020b442f78139c0
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-11 08:25:52 -0300

    [feat]: Checking for user NFT implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 import HomePage from "./components/HomePage"
 const App = () => {
   return (
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,41 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
-
+import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 export default function HomePage() {
-    const adress = useAddress();
+    const address = useAddress();
     const connectWithMetamask = useMetamask();
-    console.log(adress)
+    console.log(address)
 
+    const editionDrop = useEditionDrop("0x4Ee8e6D37B8D1a049a0c795A65962b9921D6cf7B");
+
+    
+    const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   
-    if (!adress) {
+    useEffect(() => {
+      // Se ele não tiver uma carteira conectada, saia!
+      if (!address) {
+        return;
+      }
+      
+      const checkBalance = async () => {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0);
+        // Se o saldo for maior do que 0, ele tem nosso NFT!
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 esse usuário tem o NFT de membro!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 esse usuário NÃO tem o NFT de membro.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Falha ao ler saldo", error);
+      }
+    };
+    checkBalance();
+    }, [address, editionDrop]);
+  
+    if (!address) {
         return (
             < div className = "landing" >
             <h1>Bem-vind@s à TPJDAO - a DAO dos tenistas de João Pessoa</h1>
