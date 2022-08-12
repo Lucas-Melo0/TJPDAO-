47b789369919088161392d4c1e7dd4f3af0a8192
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-12 11:36:50 -0300

    [feat]: Added airdrop functionality to NFT holders

---
+++ scripts/7-airdrop-token.js
@@ -0,0 +1,43 @@
+import sdk from "./1-initialize-sdk.js";
+
+
+const editionDrop = sdk.getEditionDrop("0x4Ee8e6D37B8D1a049a0c795A65962b9921D6cf7B");
+
+// Esse é o endereço do nosso contrato ERC-20 do nosso token.
+const token = sdk.getToken("0x25f611aEdb913b39e4eCFE8B5a706e91921B6f99");
+
+(async () => {
+  try {
+    
+    const walletAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+  
+    if (walletAddresses.length === 0) {
+      console.log(
+        "Ninguém mintou o NFT ainda, peça para alguns amigos fazerem isso e ganhar um NFT de graça!",
+      );
+      process.exit(0);
+    }
+    
+    
+    const airdropTargets = walletAddresses.map((address) => {
+      
+      const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+      console.log("✅ Vai enviar", randomAmount, "tokens para ", address);
+      
+      
+      const airdropTarget = {
+        toAddress: address,
+        amount: randomAmount,
+      };
+  
+      return airdropTarget;
+    });
+    
+    
+    console.log("🌈 Começando o airdrop...")
+    await token.transferBatch(airdropTargets);
+    console.log("✅ Feito o airdrop de tokens para todos os donos de NFT!");
+  } catch (err) {
+    console.error("O airdrop de tokens falhou", err);
+  }
+})();
